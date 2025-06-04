f8a8a89afe1a5b4b99101212aede22aac038dc50
rustam-1108d <133682982+rustam-1108d@users.noreply.github.com>
2025-06-05 03:35:38 +0500

    Fix app/config/config.cjs

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,17 +1,9 @@
 require('dotenv').config();
 
 module.exports = {
-  // development: {
-  //   dialect: 'sqlite',
-  //   storage: './database.sqlite',
-  // },
   development: {
-    dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
+    dialect: 'sqlite',
+    storage: './database.sqlite',
   },
   production: {
     dialect: 'postgres',
