a1575e298be092c1435e82bcc6a15541b3a229e1
rustam-1108d <133682982+rustam-1108d@users.noreply.github.com>
2025-06-04 23:06:06 +0500

    Step 6.1 - Edit app/config/config.cjs

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,9 +1,17 @@
 require('dotenv').config();
 
 module.exports = {
+  // development: {
+  //   dialect: 'sqlite',
+  //   storage: './database.sqlite',
+  // },
   development: {
-    dialect: 'sqlite',
-    storage: './database.sqlite',
+    dialect: 'postgres',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
   },
   production: {
     dialect: 'postgres',
@@ -13,16 +21,16 @@ module.exports = {
     port: process.env.DATABASE_PORT,
     host: process.env.DATABASE_HOST,
   },
-  test: {
-    dialect: 'sqlite',
-    storage: './database.test.sqlite',
-  },
   // test: {
-  //   dialect: 'postgres',
-  //   database: process.env.DATABASE_NAME,
-  //   username: process.env.DATABASE_USERNAME,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   port: process.env.DATABASE_PORT,
-  //   host: process.env.DATABASE_HOST,
+  //   dialect: 'sqlite',
+  //   storage: './database.test.sqlite',
   // },
+  test: {
+    dialect: 'postgres',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
+  },
 };
